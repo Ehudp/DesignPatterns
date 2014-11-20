52c061fbc7c5a1c1875cc6d07391b50f7d71c75e
ehudp <Ehud.pinhas@gmail.com>
2014-11-20 15:52:54 +0200

    add viewmodelbase

---
+++ DesignPatternsWpf/ViewModels/DesignPatternViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWpf.ViewModels
+{
+    public class DesignPatternViewModel:ViewModelBase
+    {
+       
+        public string Name
+        {
+            get { return GetProperty<string>(); ; }
+            set { SetProperty(value, () => Name); }
+        }
+    }
+}
---
+++ DesignPatternsWpf/ViewModels/ViewModelBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatternsWpf.Annotations;
+
+namespace DesignPatternsWpf.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region DataMembers
+
+        private Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+        #endregion
+
+        #region Properties
+
+        protected Dictionary<string, object> Properties
+        {
+            get { return _properties; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanged<T>(params Expression<Func<T>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                MemberExpression memberExpression =
+                    (MemberExpression)expression.Body;
+                OnPropertyChanged(memberExpression.Member.Name);
+            }
+        }
+
+        protected virtual T GetProperty<T>([CallerMemberName] string propertyName = null)
+        {
+            if (!_properties.ContainsKey(propertyName))
+            {
+                _properties.Add(propertyName, default(T));
+            }
+            return (T)_properties[propertyName];
+        }
+
+        protected virtual bool SetProperty<T>(T value, Expression<Func<T>> currentExpression,
+                                              params Expression<Func<T>>[] otherExpressions)
+        {
+            MemberExpression memberExpression =
+                       (MemberExpression)currentExpression.Body;
+            var propertyName = memberExpression.Member.Name;
+            var oldValue = GetProperty<T>(propertyName);
+            var changed = !EqualityComparer<T>.Default.Equals(oldValue, value);
+
+            if (changed)
+            {
+                _properties[propertyName] = value;
+                OnPropertyChanged(propertyName);
+                OnPropertyChanged(otherExpressions);
+            }
+
+            return changed;
+        }
+   
+        #endregion
+
+    }
+
+}
